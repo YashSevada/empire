8f408c729d87319b0eb430460a4e7e77baac3444
kunjirprajakta5 <kunjirprajakta5@gmail.com>
2021-07-18 20:14:59 +0530

    Updates

---
+++ precache-manifest.5d9d5e7c3593a6bf0538260c81c9b230.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea846b2556057e48023ffce45811ab2e",
+    "url": "/empire/index.html"
+  },
+  {
+    "revision": "4155a62b6be6320b12c7",
+    "url": "/empire/static/css/main.d6d2e905.chunk.css"
+  },
+  {
+    "revision": "cb170ed7f435afc85587",
+    "url": "/empire/static/js/2.27505fc1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/empire/static/js/2.27505fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4155a62b6be6320b12c7",
+    "url": "/empire/static/js/main.443120fe.chunk.js"
+  },
+  {
+    "revision": "61d6aa72c4565e923b15",
+    "url": "/empire/static/js/runtime-main.bdd1b2d3.js"
+  },
+  {
+    "revision": "568842c00c7d9bdaa1be469a404e7938",
+    "url": "/empire/static/media/archi.568842c0.jpg"
+  },
+  {
+    "revision": "c6bdcc96cc36612d5702f4131e854fee",
+    "url": "/empire/static/media/baug (1).c6bdcc96.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/empire/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/empire/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/empire/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "0d0e6f8dc53a7e5c42b62040eee5e6c8",
+    "url": "/empire/static/media/logicsitic.0d0e6f8d.jpg"
+  },
+  {
+    "revision": "55b45ff43010c1fd914befc39410eb7f",
+    "url": "/empire/static/media/logo.55b45ff4.jpg"
+  },
+  {
+    "revision": "8fee612eb9611708820e12c9d7c354d2",
+    "url": "/empire/static/media/logo1.8fee612e.jpg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/empire/static/media/video-placeholder.a622fc5d.jpg"
+  },
+  {
+    "revision": "f06387ee6b782ebaf14bf578f5ecfe41",
+    "url": "/empire/static/media/web.f06387ee.jpg"
+  }
+]);
