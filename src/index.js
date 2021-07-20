305d35c76e7c141fe9014fde61b0b7f6df5b5a6f
kunjirprajakta5 <kunjirprajakta5@gmail.com>
2021-07-20 14:28:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
-import App from './App';
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 
 //import './App.css';
